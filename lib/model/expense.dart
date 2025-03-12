a75effcd383086a457e5cec8deb8ab7ea31a8c81
jlisiecki <jacek.lisiecki@primaris.eu>
2025-03-12 21:15:34 +0100

    expenses view init

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: constant_identifier_names
 
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
+final formatter = DateFormat.yMd();
 const uuid = Uuid();
 
 enum Category {
@@ -10,6 +13,13 @@ enum Category {
   LEISURE,
   WORK,
 }
+
+const categoryIcons = {
+  Category.FOOD: Icons.lunch_dining,
+  Category.TRAVEL: Icons.flight_takeoff,
+  Category.LEISURE: Icons.movie,
+  Category.WORK: Icons.work
+};
 
 class Expense {
   Expense({
@@ -24,4 +34,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
