c0dd801fcb4bdf13bfae9bdc9473f9ecca866b02
jlisiecki <jacek.lisiecki@primaris.eu>
2025-03-15 20:42:49 +0100

    styling for light and dark mode

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -10,11 +10,17 @@ class ExpensesList extends StatelessWidget {
   final void Function(Expense expense) onRemoveExpense;
 
   @override
-  Widget build(BuildContext) {
+  Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withAlpha(75),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
         onDismissed: (direction) {
           onRemoveExpense(expenses[index]);
         },
